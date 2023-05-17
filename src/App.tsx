2d26c19fd2a30fc7f018ff8f2897ecc73bbd06fa
Haktan Albayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2023-05-17 12:06:39 +0300

    Mui Chip

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import { MuiBottomNavigation } from './components/MuiBottomNavigation';
 import { MuiAvatar } from './components/MuiAvatar';
 import { MuiBadge } from './components/MuiBadge';
 import { MuiList } from './components/MuiList';
+import { MuiChip } from './components/MuiChip';
 
 function App() {
   return (
@@ -47,6 +48,7 @@ function App() {
       <MuiAvatar />
       <MuiBadge />
       <MuiList />
+      <MuiChip />
     </div>
   );
 }
